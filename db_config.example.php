fc6d99f13d277f3667980c6b303b716dcbcdc704
Alban P <albanpirraud@hotmail.fr>
2024-12-05 16:02:00 +0100

    Ajout du fichier db_config.example.php

---
+++ db_config.example.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'database' => [
+        'host' => 'host_name', // ex : localhost
+        'port' => 'port_number',
+        'db_name' => 'db_name',
+        'charset' => 'charset_code' // ex : utf8mb4
+    ],
+    'user' => [
+        'password' => 'db_password',
+        'username' => 'db_username',
+    ]
+];
