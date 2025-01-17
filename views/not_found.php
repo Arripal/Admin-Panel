316430043fba526cff49bb4878c35e60c7d419eb
Alban P <albanpirraud@hotmail.fr>
2025-01-17 10:44:18 +0100

    Mise à jour des vues

--- views/not_found.php
+++ views/not_found.php
@@ -1,29 +1,16 @@
 <?php
 access_view('/components/head.view', ['title' => 'Ressource introuvable']);
 ?>
-
-<?php
-access_view('/components/header.view');
-?>
-<div class="container">
-    <?php
-    access_view('/components/sidebar.view');
-    ?>
-    <main class="content">
-        <h2>La page demandée n'existe pas </h2>
-        <?php if (isset($errors)) { ?>
-            <div class="error">
-                <?php foreach ($errors as $error): ?>
-                    <p class="error-txt"><?= htmlspecialchars($error) ?></p>
-                <?php endforeach; ?>
-            </div>
-        <?php } ?>
-        <div>
-            <a href="/admin/dashboard">Retourner à l'accueil</a>
+<section class="bg-white dark:bg-gray-900 min-h-[100dvh] flex-1 flex items-center justify-center">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
+            <h1 class="mb-4 text-9xl tracking-tight font-bold text-gray-900 dark:text-white">404</h1>
+            <h2 class="mb-4 text-3xl tracking-tight font-bold text-gray-900 dark:text-white">La page demandée n'existe pas </h2>
+            <a class="p-2 text-xl font-normal text-gray-900 rounded-lg dark:text-blue-600 hover:bg-gray-100 dark:hover:bg-gray-700 group" href="/admin/dashboard">Retourner à l'accueil</a>
         </div>
-    </main>
 
-</div>
+    </div>
+</section>
 <?php
 access_view('/components/footer.view');
 ?>
